4006c06b548f690b89905e1c2c4c025b2075f7fc
sammi morrison <ms.samantha.marie.86@gmail.com>
2020-05-09 17:18:49 -0400

    added loop for color changing based on hours but is not working properly

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,5 +1,28 @@
 var currentDateAndTime = Date(Date.now());
 
-var currentHour = new Date().getHours();
+var currentTime = new Date().getHours();
 
 $("#currentDay").append(currentDateAndTime);
+
+var presentHour = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
+updateHour();
+//loop through currentHour blocks and change color according to currentTime
+function updateHour() {
+    var currentHour = new Date().getHours();
+    console.log(currentHour);
+    for (var i = 0; i < presentHour.length; i++) {
+
+        if (parseInt(presentHour[i]) < currentHour) {
+            $("#" + presentHour[i]).attr("style", "background-color: #CED9DD");
+        }
+        else if (parseInt(presentHour[i]) > currentHour) {
+            $("#" + presentHour[i]).attr("style", "background-color: #33CEFF");
+        }
+        else if (parseInt(presentHour[i]) == currentHour) {
+            $("#" + presentHour[i]).attr("style", "background-color: #38F9FF");
+
+        }
+    }
+}
+
+//
